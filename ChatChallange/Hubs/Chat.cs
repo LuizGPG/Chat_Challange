058ca0fb2dbe36434ee18e53b526b2a7413c8aad
Luiz Guimarães <luizperesg@gmail.com>
2023-04-03 16:58:08 -0300

    create project base

---
+++ ChatChallange/Hubs/Chat.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatChallange.Hubs
+{
+    public class Chat : Hub
+    {
+        public async Task SendMessage(string usuario, string mensagem)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", usuario, mensagem);
+        }
+    }
+}
---
+++ ChatChallange/Startup.cs
@@ -0,0 +1,25 @@
+﻿using ChatChallange.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatChallange
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) => services.AddSignalR();
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<Chat>("/chat");
+            });
+        }
+    }
+}
