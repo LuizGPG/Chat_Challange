95ce153a4c41ce1fecc3085c6cbf012dc776ffa7
Luiz Guimarães <luizperesg@gmail.com>
2023-04-04 12:19:21 -0300

    create chat base

---
+++ ChatChallange/Hubs/ChatHub.cs
@@ -0,0 +1,94 @@
+﻿using ChatChallange.Domain.Model;
+using ChatChallange.Service.Interface;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatChallange.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly IStooqService _stooqService;
+        private readonly IUserChatService _userChatService;
+        //private readonly IDictionary<string, UserConnection> _connections;
+
+        public ChatHub(IStooqService stooqService, IUserChatService userChatService/*, IDictionary<string, UserConnection> connections*/)
+        {
+            _stooqService = stooqService;
+            _userChatService = userChatService;
+            //_connections = connections;
+        }
+
+        //public override Task OnDisconnectedAsync(Exception exception)
+        //{
+        //    if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+        //    {
+        //        _connections.Remove(Context.ConnectionId);
+        //        Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", "Chat Bot", $"{userConnection.User} has left");
+        //        SendUsersConnected(userConnection.Room);
+        //    }
+
+        //    return base.OnDisconnectedAsync(exception);
+        //}
+
+        //public async Task JoinRoom(UserConnection userConnection)
+        //{
+        //    await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+        //    _connections[Context.ConnectionId] = userConnection;
+
+        //    await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", "Chat Bot", $"{userConnection.User} has joined {userConnection.Room}");
+
+        //    await SendUsersConnected(userConnection.Room);
+        //}
+
+        //public async Task SendMessage(string message)
+        //{
+        //    if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+        //    {
+        //        await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
+        //    }
+        //}
+
+        //public Task SendUsersConnected(string room)
+        //{
+        //    var users = _connections.Values
+        //        .Where(c => c.Room == room)
+        //        .Select(c => c.User);
+
+        //    return Clients.Group(room).SendAsync("UsersInRoom", users);
+        //}
+
+
+        public async Task SendMessage(string user, string mensagem)
+        {
+            var anwser = await _stooqService.CallEndpointStooq(mensagem);
+            await _userChatService.SaveMessage(1 /*USER ID*/,mensagem, anwser);
+
+            await GetUserChatQueue(user);
+        }
+
+        private async Task GetUserChatQueue(string user)
+        {
+            var userChat = _userChatService.GetUserChatQueue(user);
+            await Clients.All.SendAsync("ReceiveMessage", "User", userChat.Message);
+            await Clients.All.SendAsync("ReceiveMessage", "Chat Bot", userChat.Anwser);
+            await Clients.User(user).SendAsync("ReceiveMessage", "Chat Bot", userChat.Anwser);
+        }
+
+        //fazer metodo que fica escutando fila e valida se a mensagem foi para o usuario em questao
+
+        public async Task LoadChat(string usuario)
+        {
+            var messages = await _userChatService.GetAllByUserId(1);
+
+            foreach (var message in messages)
+            {
+                await Clients.All.SendAsync("ReceiveMessage", "User", message.Message);
+                await Clients.All.SendAsync("ReceiveMessage", "Chat Bot", message.Anwser);
+            }
+        }
+    }
+}
--- ChatChallange/Startup.cs
+++ ChatChallange/Startup.cs
@@ -5,36 +5,51 @@ using ChatChallange.Service;
 using ChatChallange.Service.Interface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web.UI;
 
 namespace ChatChallange
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
+        public IConfiguration _configuration { get; }
 
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public void ConfigureServices(IServiceCollection services) { 
+        public void ConfigureServices(IServiceCollection services)
+        {
             services.AddSignalR();
 
             services.AddDbContext<ApplicationContext>(options =>
-              options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+              options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"))
               .EnableSensitiveDataLogging(true));
-            // services
-            services.AddScoped<IStooqService, StooqService>();
-            services.AddScoped<IQueueService, QueueService>();
-            services.AddScoped<IUserChatService, UserChatService>();
 
-            // repos
-            services.AddScoped<IUserChatRepository, UserChatRepository>();
+            services.AddDbContext<IdentityContext>(options =>
+              options.UseSqlServer(_configuration.GetConnectionString("IdentityConnection"))
+              .EnableSensitiveDataLogging(true));
+
+
+            IocConfig(services);
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<IdentityContext>();
+            
+            services.AddAuthorization(options =>
+            {
+                // By default, all incoming requests will be authorized according to the default policy
+                options.FallbackPolicy = options.DefaultPolicy;
+            });
+
+            services.AddRazorPages()
+                .AddMvcOptions(options => { })
+                .AddMicrosoftIdentityUI();
         }
-
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDefaultFiles();
@@ -42,10 +57,32 @@ namespace ChatChallange
 
             app.UseRouting();
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            //app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapHub<Chat>("/chat");
+                endpoints.MapRazorPages();
+                endpoints.MapHub<ChatHub>("/ChatHub");
+                endpoints.MapControllers();
+
             });
+        }
+
+        private static void IocConfig(IServiceCollection services)
+        {
+            // services
+            services.AddScoped<IStooqService, StooqService>();
+            services.AddScoped<IQueueService, QueueService>();
+            services.AddScoped<IUserChatService, UserChatService>();
+
+            // repos
+            services.AddScoped<IUserChatRepository, UserChatRepository>();
         }
     }
 }
